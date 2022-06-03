e93f3a05429f3cb6d72cceab1946da1e611fa077
tejuhl <tejuhl@utu.fi>
2022-06-03 15:01:47 +0300

    ownchallenge class

--- src/main/java/com/vaadin/vaadin_archetype_application/BallContainer.java
+++ src/main/java/com/vaadin/vaadin_archetype_application/BallContainer.java
@@ -6,21 +6,31 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("mytheme")
-public class BallTest extends HorizontalLayout {
+public class BallContainer extends HorizontalLayout {
     HorizontalLayout answerLine = new HorizontalLayout();   
     VerticalLayout numLayout = new VerticalLayout();
     Label numLabel = new Label();
     
-    public BallTest(int number) {
+    public BallContainer(int number, String type, boolean visualizeBool) {
         numLabel.setCaption(Integer.toString(number));
         numLabel.setStyleName("numLabel");
         this.setSpacing(true);
         this.setStyleName("challengeLine");
         this.setWidthUndefined();
         this.setHeightUndefined();
-    
+        
+        BallVisualization ballVis = new BallVisualization(number, type);
+        ballVis.setHeightUndefined();
+        ballVis.setWidth("60px");
         numLayout.setStyleName("numLayout");
-        numLayout.addComponents(new BallVisualization(number), numLabel);
+        numLayout.setHeightUndefined();
+        numLayout.setSpacing(true);
+        if (!visualizeBool) {
+            numLayout.addComponents(new VerticalLayout() ,numLabel);
+        } else {
+            numLayout.addComponents(ballVis, numLabel);
+        }
+        
         this.addComponent(numLayout);
 
     }
---
+++ src/main/java/com/vaadin/vaadin_archetype_application/OwnChallenge.java
@@ -0,0 +1,89 @@
+package com.vaadin.vaadin_archetype_application;
+
+import java.util.ArrayList;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+
+@Theme("mytheme")
+public class OwnChallenge extends FormLayout {
+    
+    ArrayList<String> ownChallengeList = new ArrayList<String>();
+    VerticalLayout challengeListLayout = new VerticalLayout();
+
+    public OwnChallenge () {
+        HorizontalLayout addChallengeLayout = new HorizontalLayout();
+        TextField newChallengeText = new TextField("Add a new challenge");
+        Button addButton = new Button("Add");
+        addButton.setStyleName("check");
+        addButton.setClickShortcut(KeyCode.ENTER);
+        addButton.addClickListener(event -> {
+            String ch = newChallengeText.getValue();
+            addNewChallenge(ch);
+        });
+
+        addChallengeLayout.addComponents(newChallengeText, addButton);
+        addChallengeLayout.setSpacing(true);
+        addChallengeLayout.setComponentAlignment(addButton, Alignment.BOTTOM_LEFT);
+
+        HorizontalLayout formButtonBox = new HorizontalLayout();
+	        
+	    Button submitButton = new Button("Submit");
+	    submitButton.addStyleName("submitButton");
+        formButtonBox.addComponent(submitButton);
+	    formButtonBox.addStyleName("formButtonBox");
+
+        formButtonBox.setWidth("100%");
+	    formButtonBox.setComponentAlignment(submitButton, Alignment.MIDDLE_CENTER);
+
+        challengeListLayout.setStyleName("challengeListLayout");
+
+        addComponents(addChallengeLayout, challengeListLayout, formButtonBox);
+        setComponentAlignment(challengeListLayout, Alignment.MIDDLE_CENTER);
+        setSpacing(true);
+    }
+
+
+    private HorizontalLayout ownChallengeLine (String challenge, int index) {
+        HorizontalLayout challengeLine = new HorizontalLayout();
+        Label challengeLabel = new Label(challenge);
+        Button deleteButton = new Button();
+        deleteButton.setIcon(FontAwesome.TRASH);
+
+        challengeLine.setSpacing(true);
+        challengeLine.addComponents(challengeLabel, deleteButton);
+        deleteButton.addClickListener(e -> {
+            ownChallengeList.remove(index);
+            updateChallengeList();
+        });
+
+
+        return challengeLine;
+    }
+
+    private void addNewChallenge(String challengeText) {
+        ownChallengeList.add(challengeText);
+        updateChallengeList();
+    }
+
+
+    private void updateChallengeList() {
+        challengeListLayout.removeAllComponents();
+        for (int i = 0; i < ownChallengeList.size(); i++) {
+            HorizontalLayout challengeLine = ownChallengeLine(ownChallengeList.get(i), i);
+
+            challengeListLayout.addComponent(challengeLine);
+            challengeListLayout.setComponentAlignment(challengeLine, Alignment.TOP_CENTER);
+
+        }
+    }
+}
